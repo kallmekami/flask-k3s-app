c5d22f9e4901cab57f098ba84cb1184a945c19c1
kallmekami <kvmykvze@gmail.com>
2026-02-26 15:36:08 +0330

    self host runner

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ app=Flask(__name__)
 
 @app.route('/')
 def hello():
-     return f"سلام! من رو k3s دارم اجرا میشم. ورژن: {os.environ.get('VERSION', '1.0')}"
+     return f"hello: {os.environ.get('VERSION', '1.0')}"
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
